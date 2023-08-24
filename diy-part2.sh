62cdaa8075435d469a758f203a7ad93fc6a1b9dd
Shanxia.OrangeMas <datoubuchou@foxmail.com>
2023-08-24 20:34:17 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -9,3 +9,19 @@
 # File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
+
+#Git Theme
+cd package/feeds/luci/
+rm -rf luci-theme-argon
+rm -rf luci-theme-argon-mod
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git luci-theme-argon
+cd ..
+cd ..
+cd ..
+
+# Modify default IP
+sed -i 's/192.168.1.1/192.168.31.1/g' package/base-files/files/bin/config_generate
+# Themes Default
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+# AP Name Default
+sed -i 's/OpenWrt/XiaoMi-R3G/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
