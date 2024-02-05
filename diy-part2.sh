7bb09983c965521e9f00a48bef97d797d1fedec9
Shanxia.OrangeMas <datoubuchou@foxmail.com>
2024-02-04 21:15:51 -0900

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -32,7 +32,8 @@ sed -i 's/PKG_BUILD_FLAGS:=no-mips16/PKG_USE_MIPS16:=0/g' tailscale/Makefile
 cd ..
 cd ..
 cd ..
-
+# Rust
+sed -i 's/PKG_BUILD_FLAGS:=no-mips16/PKG_USE_MIPS16:=0/g' feeds/packages/lang/rust/Makefile
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.31.1/g' package/base-files/files/bin/config_generate
 # Themes Default
