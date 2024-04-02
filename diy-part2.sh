5bcb348ebd9b4f6722099e6216b6df869357a916
Shanxia.OrangeMas <datoubuchou@foxmail.com>
2024-04-02 10:59:57 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,34 +11,40 @@
 #
 
 #Git Theme
-# cd feeds/luci/themes
-# rm -rf luci-theme-argon
-# rm -rf luci-theme-argon-mod
+cd feeds/luci/themes
+rm -rf luci-theme-argon
+rm -rf luci-theme-argon-mod
+rm -rf luci-theme-design
 # git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git luci-theme-argon
-# cd ..
-# cd ..
-# cd ..
-git clone https://github.com/gngpp/luci-theme-design.git  package/luci-theme-design
+git clone https://github.com/gngpp/luci-theme-design.git  luci-theme-design
+cd ..
+cd ..
+cd ..
+
 
 
 #Git FRP
-cd feeds/packages/net
-rm -rf frp
-rm -rf tailscale
+cd feeds/packages
+rm -rf net/frp
+rm -rf net/tailscale
+rm -rf lang/rust
+rm -rf lang/golang
 git clone https://github.com/immortalwrt/packages
-cp -r packages/net/frp ./
-cp -r packages/net/tailscale ./
+cp -r packages/net/frp net/
+cp -r packages/net/tailscale net/
+cp -r packages/lang/golang lang/
+cp -r packages/lang/rust lang/
 rm -rf packages
-sed -i 's/PKG_BUILD_FLAGS:=no-mips16/PKG_USE_MIPS16:=0/g' frp/Makefile
-sed -i 's/PKG_BUILD_FLAGS:=no-mips16/PKG_USE_MIPS16:=0/g' tailscale/Makefile
+sed -i 's/PKG_BUILD_FLAGS:=no-mips16/PKG_USE_MIPS16:=0/g' net/frp/Makefile
+sed -i 's/PKG_BUILD_FLAGS:=no-mips16/PKG_USE_MIPS16:=0/g' net/tailscale/Makefile
+sed -i 's/PKG_BUILD_FLAGS:=no-mips16/PKG_USE_MIPS16:=0/g' lang/golang/golang/Makefile
+sed -i 's/PKG_BUILD_FLAGS:=no-mips16/PKG_USE_MIPS16:=0/g' lang/rust/Makefile
 cd ..
 cd ..
-cd ..
-# Rust
-sed -i 's/PKG_BUILD_FLAGS:=no-mips16/PKG_USE_MIPS16:=0/g' feeds/packages/lang/rust/Makefile
+
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.31.1/g' package/base-files/files/bin/config_generate
 # Themes Default
-sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+sed -i 's/luci-theme-bootstrap/luci-theme-design/g' feeds/luci/collections/luci/Makefile
 # AP Name Default
 sed -i 's/OpenWrt/XiaoMi-R3G/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
