d87cb6ee3be964cc631f975ccf282430bdf41895
Shanxia.OrangeMas <datoubuchou@foxmail.com>
2024-04-06 09:49:51 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -16,7 +16,7 @@ rm -rf luci-theme-argon
 rm -rf luci-theme-argon-mod
 rm -rf luci-theme-design
 # git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git luci-theme-argon
-git clone https://github.com/gngpp/luci-theme-design.git  luci-theme-design
+git clone --depth=1 https://github.com/gngpp/luci-theme-design.git  luci-theme-design
 cd ..
 cd ..
 cd ..
@@ -29,7 +29,7 @@ rm -rf net/frp
 rm -rf net/tailscale
 rm -rf lang/rust
 rm -rf lang/golang
-git clone https://github.com/immortalwrt/packages
+git clone --depth=1 https://github.com/immortalwrt/packages
 cp -r packages/net/frp net/
 cp -r packages/net/tailscale net/
 cp -r packages/lang/golang lang/
@@ -48,3 +48,12 @@ sed -i 's/192.168.1.1/192.168.31.1/g' package/base-files/files/bin/config_genera
 sed -i 's/luci-theme-bootstrap/luci-theme-design/g' feeds/luci/collections/luci/Makefile
 # AP Name Default
 sed -i 's/OpenWrt/XiaoMi-R3G/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+
+# Clash Meta Core
+curl -sL -m 30 --retry 2 https://raw.githubusercontent.com/vernesong/OpenClash/core/master/meta/clash-linux-mipsle-softfloat.tar.gz -o /tmp/clash.tar.gz
+tar zxvf /tmp/clash.tar.gz -C /tmp
+chmod +x /tmp/clash
+mkdir -p ./feeds/openclash/luci-app-openclash/root/etc/openclash/core
+mv /tmp/clash ./feeds/openclash/luci-app-openclash/root/etc/openclash/core/clash_meta
+rm -rf /tmp/clash.tar.gz
+
