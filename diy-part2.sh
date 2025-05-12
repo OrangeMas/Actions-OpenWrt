6431b5453b43319f44396024f2a9d086eba52c3b
Shanxia.OrangeMas <datoubuchou@foxmail.com>
2025-05-13 01:12:43 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -9,51 +9,50 @@
 # File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
-
-#Git Theme
-cd feeds/luci/themes
-rm -rf luci-theme-argon
-rm -rf luci-theme-argon-mod
-rm -rf luci-theme-design
-# git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git luci-theme-argon
-git clone --depth=1 https://github.com/gngpp/luci-theme-design.git  luci-theme-design
-cd ..
-cd ..
-cd ..
-
+# #Git Theme
+# cd feeds/luci/themes
+# rm -rf luci-theme-argon
+# rm -rf luci-theme-argon-mod
+# rm -rf luci-theme-design
+# # git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git luci-theme-argon
+# git clone --depth=1 https://github.com/gngpp/luci-theme-design.git  luci-theme-design
+# cd ..
+# cd ..
+# cd ..
 
 
-#Git FRP
-cd feeds/packages
-rm -rf net/frp
-rm -rf net/tailscale
-rm -rf lang/rust
-rm -rf lang/golang
-git clone --depth=1 https://github.com/immortalwrt/packages
-cp -r packages/net/frp net/
-cp -r packages/net/tailscale net/
-cp -r packages/lang/golang lang/
-cp -r packages/lang/rust lang/
-rm -rf packages
-sed -i 's/PKG_BUILD_FLAGS:=no-mips16/PKG_USE_MIPS16:=0/g' net/frp/Makefile
-sed -i 's/PKG_BUILD_FLAGS:=no-mips16/PKG_USE_MIPS16:=0/g' net/tailscale/Makefile
-sed -i 's/PKG_BUILD_FLAGS:=no-mips16/PKG_USE_MIPS16:=0/g' lang/golang/golang/Makefile
-sed -i 's/PKG_BUILD_FLAGS:=no-mips16/PKG_USE_MIPS16:=0/g' lang/rust/Makefile
-cd ..
-cd ..
+
+# #Git FRP
+# cd feeds/packages
+# rm -rf net/frp
+# rm -rf net/tailscale
+# rm -rf lang/rust
+# rm -rf lang/golang
+# git clone --depth=1 https://github.com/immortalwrt/packages
+# cp -r packages/net/frp net/
+# cp -r packages/net/tailscale net/
+# cp -r packages/lang/golang lang/
+# cp -r packages/lang/rust lang/
+# rm -rf packages
+# sed -i 's/PKG_BUILD_FLAGS:=no-mips16/PKG_USE_MIPS16:=0/g' net/frp/Makefile
+# sed -i 's/PKG_BUILD_FLAGS:=no-mips16/PKG_USE_MIPS16:=0/g' net/tailscale/Makefile
+# sed -i 's/PKG_BUILD_FLAGS:=no-mips16/PKG_USE_MIPS16:=0/g' lang/golang/golang/Makefile
+# sed -i 's/PKG_BUILD_FLAGS:=no-mips16/PKG_USE_MIPS16:=0/g' lang/rust/Makefile
+# cd ..
+# cd ..
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.31.1/g' package/base-files/files/bin/config_generate
 # Themes Default
-sed -i 's/luci-theme-bootstrap/luci-theme-design/g' feeds/luci/collections/luci/Makefile
+sed -i 's/luci-theme-bootstrap/luci-theme-design/g' feeds/luci/collections/luci-light/Makefile
 # AP Name Default
-sed -i 's/OpenWrt/XiaoMi-R3G/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+# sed -i 's/OpenWrt/XiaoMi-R3G/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 
 # Clash Meta Core
-# curl -sL -m 30 --retry 2 https://raw.githubusercontent.com/vernesong/OpenClash/core/master/meta/clash-linux-mipsle-softfloat.tar.gz -o /tmp/clash.tar.gz
-# tar zxvf /tmp/clash.tar.gz -C /tmp
-# chmod +x /tmp/clash
-# mkdir -p ./feeds/openclash/luci-app-openclash/root/etc/openclash/core
-# mv /tmp/clash ./feeds/openclash/luci-app-openclash/root/etc/openclash/core/clash_meta
-# rm -rf /tmp/clash.tar.gz
+curl -sL -m 30 --retry 2 https://raw.githubusercontent.com/vernesong/OpenClash/core/master/meta/clash-linux-amd64.tar.gz -o /tmp/clash.tar.gz
+tar zxvf /tmp/clash.tar.gz -C /tmp
+chmod +x /tmp/clash
+mkdir -p ./feeds/openclash/luci-app-openclash/root/etc/openclash/core
+mv /tmp/clash ./feeds/openclash/luci-app-openclash/root/etc/openclash/core/clash_meta
+rm -rf /tmp/clash.tar.gz
 
